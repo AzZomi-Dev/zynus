75f524e0b68a2d96537d0a1909b9f5b8f104c7c3
azz <abdulazim.awad.khalifa@gmail.com>
2026-07-05 17:20:03 +0300

    chore: bump version to 1.0.5

--- sandbox/server.py
+++ sandbox/server.py
@@ -17,7 +17,7 @@ import tempfile
 
 from fastapi import FastAPI
 
-app = FastAPI(title="zynus", version="1.0.4")
+app = FastAPI(title="zynus", version="1.0.5")
 
 
 async def execute_code(code: str) -> dict:
